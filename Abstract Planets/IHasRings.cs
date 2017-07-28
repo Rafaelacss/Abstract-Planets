280f30fed95085f032b8d528a10eb6e43f6e8d7c
rafaelacss <rsilvei2@my.centennialcollege.ca>
2017-07-28 14:07:50 -0400

    IHasRings interface implemented

---
+++ Abstract Planets/IHasRings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Rafaela Silveira
+ * Date: July 28, 2017
+ * Description: IHasRings interface - include HasRings method that returns a bool
+ * Version: 0.1
+ */
+
+namespace Abstract_Planets
+{
+    interface IHasRings
+    {
+        bool HasRings();
+    }
+}
