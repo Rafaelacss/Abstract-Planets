da9da6484b0106d6b859afdd9e971b27e5a6ab77
rafaelacss <rsilvei2@my.centennialcollege.ca>
2017-07-28 14:44:38 -0400

    Main method structure

--- Abstract Planets/Program.cs
+++ Abstract Planets/Program.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Rafaela Silveira
+ * Date: July 28, 2017
+ * Version: 0.1
+ */
+
 namespace Abstract_Planets
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 69911, 317.8, "Gas");
+            Console.WriteLine(giantPlanet.ToString());
 
+            TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Earth", 40075.017, 5.97237, true);
+            Console.WriteLine(terrestrialPlanet.ToString());
+
+            WaitForAnyKey();
+        }
+
+        public static void WaitForAnyKey()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
