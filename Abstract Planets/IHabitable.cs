865ec0c20d0b48f42adc76910cbf16d462f08dc3
rafaelacss <rsilvei2@my.centennialcollege.ca>
2017-07-28 14:11:00 -0400

    IHabitable interface implemented

---
+++ Abstract Planets/IHabitable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Rafaela Silveira
+ * Date: July 28, 2017
+ * Description: IHabitable interface - include Habitable method that returns a bool
+ * Version: 0.1
+ */
+
+namespace Abstract_Planets
+{
+    interface IHabitable
+    {
+        bool Habitable();
+    }
+}
