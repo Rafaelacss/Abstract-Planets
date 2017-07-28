952ad511d45ff4c5572342508f17d4e10bca67e8
rafaelacss <rsilvei2@my.centennialcollege.ca>
2017-07-28 14:04:37 -0400

    IHasMoons interface implemented

---
+++ Abstract Planets/IHasMoons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Rafaela Silveira
+ * Date: July 28, 2017
+ * Description: IHasMoons interface - include HasMoons method that returns a bool
+ * Version: 0.1
+ */
+
+namespace Abstract_Planets
+{
+    public interface IHasMoons
+    {
+        bool HasMoons();
+    }
+}
